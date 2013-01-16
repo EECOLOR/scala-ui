6929cbcd01fc1487e2ea703cab9272ad2c21176f
EECOLOR <eecolor@qirx.org>
2013-01-17 00:37:08 +0100

    Changed some settings

--- project/LibraryBuild.scala
+++ project/LibraryBuild.scala
@@ -1,24 +1,32 @@
 import sbt._
 import Keys._
+import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
 
 object LibraryBuild extends Build {
 
-  val appName = "uiFramework"
+  val appName = "scala-ui"
   val appVersion = "1.0"
 
   val appDependencies = Seq(
-      "org.scala-lang" % "scala-actors" % "2.10.0",
-      "org.specs2" % "specs2_2.10.0" % "1.12.1.1" % "test",
-      "org.mockito" % "mockito-all" % "1.9.5" % "test")
+    "org.scala-lang" % "scala-actors" % "2.10.0",
+    "org.specs2" % "specs2_2.10" % "1.13" % "test",
+    "org.mockito" % "mockito-all" % "1.9.5" % "test")
+
+  val defaultSettings = Seq(
+    libraryDependencies ++= appDependencies,
+    scalaVersion := "2.10.0",
+    scalacOptions += "-feature",
+    resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+      "releases" at "http://oss.sonatype.org/content/repositories/releases"))
+
+  val eclipseSettings = Seq(
+    EclipseKeys.withSource := true)
 
   lazy val root = Project(id = appName,
     base = file("."),
-    settings = Project.defaultSettings ++ Seq(
-        libraryDependencies ++= appDependencies,
-        scalaVersion := "2.10.0",
-        scalacOptions += "-feature",
-        resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-                    "releases"  at "http://oss.sonatype.org/content/repositories/releases")
-     ))
+    settings = 
+      Project.defaultSettings ++ 
+      defaultSettings ++ 
+      eclipseSettings)
 }
 
