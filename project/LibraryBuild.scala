d5ed935720755618c931a9baf26cafeb46e4187f
EECOLOR <eecolor@qirx.org>
2013-01-16 23:48:21 +0100

    Changed some versions

--- project/LibraryBuild.scala
+++ project/LibraryBuild.scala
@@ -7,15 +7,15 @@ object LibraryBuild extends Build {
   val appVersion = "1.0"
 
   val appDependencies = Seq(
-      "org.scala-lang" % "scala-actors" % "2.10.0-M7",
-      "org.specs2" % "specs2_2.10.0-M7" % "1.12.1.1" % "test",
+      "org.scala-lang" % "scala-actors" % "2.10.0",
+      "org.specs2" % "specs2_2.10.0" % "1.12.1.1" % "test",
       "org.mockito" % "mockito-all" % "1.9.5" % "test")
 
   lazy val root = Project(id = appName,
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
         libraryDependencies ++= appDependencies,
-        scalaVersion := "2.10.0-M7",
+        scalaVersion := "2.10.0",
         scalacOptions += "-feature",
         resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
                     "releases"  at "http://oss.sonatype.org/content/repositories/releases")
