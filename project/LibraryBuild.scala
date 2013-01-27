b453a876d74f89bc4d7d11719d9961717d74e2cf
EECOLOR <eecolor@qirx.org>
2013-01-27 23:50:24 +0100

    Removed java paths from build

--- project/LibraryBuild.scala
+++ project/LibraryBuild.scala
@@ -20,7 +20,9 @@ object LibraryBuild extends Build {
       "releases" at "http://oss.sonatype.org/content/repositories/releases"))
 
   val eclipseSettings = Seq(
-    EclipseKeys.withSource := true)
+    EclipseKeys.withSource := true,
+    unmanagedSourceDirectories in Compile <<= Seq(scalaSource in Compile).join,
+    unmanagedSourceDirectories in Test <<= Seq(scalaSource in Test).join)
 
   lazy val root = Project(id = appName,
     base = file("."),
